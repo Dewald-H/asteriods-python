a0b8befb88d27966f7bd29efc3fb679da934f3dc
Dewald <dewaldh04@gmail.com>
2026-01-14 20:28:38 +0200

    added basic files and logger

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print(f"Starting Asteroids with pygame version : {pygame.version.ver}")
+    print(f"Screen width: {1280}")
+    print(f"Screen height: {720}")
+
+
+if __name__ == "__main__":
+    main()
