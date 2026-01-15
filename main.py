d8b9f75d1f041841965f3e868426b179977313d4
Dewald <dewaldh04@gmail.com>
2026-01-15 18:57:19 +0200

    Added player

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     print(f"Starting Asteroids with pygame version : {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         # Log the game state
@@ -21,9 +27,11 @@ def main():
             
         # Draw
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        
 
+        # Limit FPS to 60 and calculate delta time
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
